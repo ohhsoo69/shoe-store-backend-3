c5de1255235fdbeacfffa06bc4d7749a3f74f5e6
ohhsoo69 <153300356+ohhsoo69@users.noreply.github.com>
2024-03-09 21:31:58 +0530

    Update order.ts payment fix

--- src/api/order/controllers/order.ts
+++ src/api/order/controllers/order.ts
@@ -1,64 +1,54 @@
+("use strict");
+const stripe = require("stripe")(process.env.STRIPE_KEY);
 /**
  * order controller
  */
 
-"use strict";
-import { Context } from "koa";
-import stripe from "stripe";
+const { createCoreController } = require("@strapi/strapi").factories;
 
-const stripeInstance = new stripe(process.env.STRIPE_KEY);
+module.exports = createCoreController("api::order.order", ({ strapi }) => ({
+  async create(ctx) {
+    const { products } = ctx.request.body;
+    try {
+      const lineItems = await Promise.all(
+        products.map(async (product) => {
+          const item = await strapi
+            .service("api::product.product")
+            .findOne(product.id);
 
-/**
- * order controller
- */
-import { factories } from "@strapi/strapi";
+          console.log("this is item------->", item);
+          console.log("this is product------->", product);
 
-export default factories.createCoreController(
-  "api::order.order",
-  ({ strapi }) => ({
-    async create(ctx: Context) {
-      const { products } = ctx.request.body;
-      try {
-        const lineItems = await Promise.all(
-          products.map(async (product: any) => {
-            const item: any = await strapi
-              .service("api::product.product")
-              .findOne(product.id);
-
-            console.log("this is item------->", item);
-            console.log("this is product------->", product);
-
-            return {
-              price_data: {
-                currency: "inr",
-                product_data: {
-                  name: item.name,
-                },
-                unit_amount: Math.round(item.price * 100),
+          return {
+            price_data: {
+              currency: "inr",
+              product_data: {
+                name: item.name,
               },
-              quantity: product.quantity,
-            };
-          })
-        );
+              unit_amount: Math.round(item.price * 100),
+            },
+            quantity: product.quantity,
+          };
+        })
+      );
 
-        const session = await stripeInstance.checkout.sessions.create({
-          shipping_address_collection: { allowed_countries: ["IN"] },
-          payment_method_types: ["card"],
-          mode: "payment",
-          success_url: process.env.CLIENT_URL + `/success`,
-          cancel_url: process.env.CLIENT_URL + "/failed",
-          line_items: lineItems,
-        });
+      const session = await stripe.checkout.sessions.create({
+        shipping_address_collection: { allowed_countries: ["IN"] },
+        payment_method_types: ["card"],
+        mode: "payment",
+        success_url: process.env.CLIENT_URL + `/success`,
+        cancel_url: process.env.CLIENT_URL + "/failed",
+        line_items: lineItems,
+      });
 
-        await strapi
-          .service("api::order.order")
-          .create({ data: { products, stripeId: session.id } });
+      await strapi
+        .service("api::order.order")
+        .create({ data: { products, stripeId: session.id } });
 
-        return { stripeSession: session };
-      } catch (error: any) {
-        ctx.response.status = 500;
-        return { error };
-      }
-    },
-  })
-);
+      return { stripeSession: session };
+    } catch (error) {
+      ctx.response.status = 500;
+      return { error };
+    }
+  },
+}));
